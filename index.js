589cbc03cd1509bd904f94038b98096a66cba2be
Arun Ganesh <arun.planemad@gmail.com>
2016-07-29 19:15:08 +0530

    Upgrade colors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var filteredAmenity = {};
 
 var bbox = [map.getBounds()["_sw"]["lng"], map.getBounds()["_sw"]["lat"], map.getBounds()["_ne"]["lng"], map.getBounds()["_ne"]["lat"]];
 //var bbox = [77.4368, 12.8225, 77.7564, 13.0939];
-var size = 1;
+var size = 3;
 var temp = turf.hexGrid(bbox, size);
 
 var hexgrid = {};
@@ -26,13 +26,13 @@ var filterEl = document.getElementById('feature-filter');
 var listingEl = document.getElementById('feature-listing');
 
 var availableAmenities = [
-  "ActionScript",
-  "AppleScript",
-  "Asp",
-  "BASIC",
-  "C",
-  "C++",
-  "Clojure"
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure"
 ];
 
 function normalize(string) {
@@ -41,27 +41,28 @@ function normalize(string) {
 
 function renderListings(amenities) {
     filterEl.setAttribute("placeholder", amenities[0]);
-     // listingEl.innerHTML = '';
-     //    amenities.forEach(function(amenity) {
-     //        var item = document.createElement('button');
-     //        item.addEventListener('mouseover', function() {
-     //        });
-     //        listingEl.appendChild(item);
-     //    });
+    // listingEl.innerHTML = '';
+    //    amenities.forEach(function(amenity) {
+    //        var item = document.createElement('button');
+    //        item.addEventListener('mouseover', function() {
+    //        });
+    //        listingEl.appendChild(item);
+    //    });
 }
 
 filterEl.addEventListener('keyup', function(e) {
-  var value = normalize(e.target.value);
+    var value = normalize(e.target.value);
 
-  var filtered = availableAmenities.filter(function(feature) {
-    var name = normalize(feature);
-    return name.indexOf(value) > -1;
-  });
-  renderListings(filtered);
+    var filtered = availableAmenities.filter(function(feature) {
+        var name = normalize(feature);
+        return name.indexOf(value) > -1;
+    });
+    renderListings(filtered);
 
 });
 
-map.on('load', function () {
+map.on('load', function() {
+    // Density hexgrid overlay
     map.addSource('hexSource', {
         type: 'geojson',
         data: temp
@@ -71,30 +72,23 @@ map.on('load', function () {
         'source': 'hexSource',
         "type": "fill",
         "paint": {
-        'fill-color': {
-            property: 'count',
-            stops: [
-              [0, '#FFFFFF'],
-              [3, '#D0EBBE'],
-              [6, '#B1DE93'],
-              [9, '#92D168'],
-              [12, '#73C43D'],
-              [15, '#64be28'],
-              [18, '#509820'],
-              [21, '#3C7218'],
-              [24, '#325F14'],
-              [27, '#284C10'],
-              [30, '#1E390C'],
-              [33, '#182e0a'],
-              [36, '#122207']
-            ]
-          },
-          "fill-opacity": 0.9
+            'fill-color': {
+                property: 'count',
+                stops: [
+                    [0, '#000'],
+                    [1, '#017a20'],
+                    [6, '#ede51e'],
+                    [15, '#fa4a3d']
+                ]
+            },
+            "fill-opacity": 0.4
         },
         "layout": {
             "visibility": 'none'
         }
     });
+
+    // Amenities vector tiles
     map.addSource('amenitiesSource', {
         "type": "vector",
         "url": "mapbox://amisha.4f400ilq"
@@ -103,12 +97,16 @@ map.on('load', function () {
         'id': 'amenitiesLayer',
         'source': 'amenitiesSource',
         'source-layer': 'outputgeojson',
-        "type": "fill",
+        "type": "circle",
         "paint": {
-        'fill-color': '#FFFFFF',
-        'fill-opacity': 0.5
+            'circle-color': '#fff',
+            'circle-opacity': 0.5,
+            'circle-radius': 2
         }
-    });    
+    });
+
+
+
 })
 
 var layerList = document.getElementById('menu');
@@ -118,54 +116,60 @@ for (var i = 0; i < inputs.length; i++) {
     inputs[i].onclick = showDensity;
 }
 
+var selectedAmenity;
+
 function filterByAmenity(obj) {
-  var filter = $('input[name=amenity]:checked', '#menu').attr('id');
-  if (obj.properties.amenity == filter) {
-    return true;
-  } else {
-    return false;
-  }
+    var filter = $('input[name=amenity]:checked', '#menu').attr('id');
+    selectedAmenity = filter;
+    if (obj.properties.amenity == filter) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function showDensity(filter) {
-  amenities = map.querySourceFeatures('amenitiesSource', {sourceLayer: 'outputgeojson'});
-  filteredAmenity= amenities.filter(filterByAmenity);
-  map.setLayoutProperty('hexLayer', 'visibility', 'visible');
-  countem();
-  map.getSource("hexSource").setData(hexgrid);
+    amenities = map.querySourceFeatures('amenitiesSource', {
+        sourceLayer: 'outputgeojson'
+    });
+    filteredAmenity = amenities.filter(filterByAmenity);
+    map.setLayoutProperty('hexLayer', 'visibility', 'visible');
+    map.setFilter('amenitiesLayer', ['==', 'amenity', selectedAmenity]);
+    countem();
+    map.getSource("hexSource").setData(hexgrid);
 
 }
-function countem(){
-  hexgrid = turf.hexGrid(bbox, size);
-  var count = 0;
-  for(var x=0;x<hexgrid.features.length;x++){
-  hexgrid.features[x].properties.count=0;}
-  console.log(hexgrid.features.length);
-  for(var y=0;y<Object.keys(hexgrid.features).length;){
-     count = 0;
-  for(var c=0;c<filteredAmenity.length;c++){
-  var poly=turf.polygon(hexgrid.features[y].geometry.coordinates);
-  if(filteredAmenity[c].geometry["type"] == "Polygon" ) {
-      if(turf.inside(turf.centroid(filteredAmenity[c]),poly)) {
-        hexgrid.features[y].properties.count += 1;
-        count++;
-    }
-  }
 
-  if(filteredAmenity[c].geometry["type"] == "Point")
-  {
-    if(turf.inside(filteredAmenity[c],poly)){
-      hexgrid.features[y].properties.count += 1;
-      count++;
+function countem() {
+    hexgrid = turf.hexGrid(bbox, size);
+    var count = 0;
+    for (var x = 0; x < hexgrid.features.length; x++) {
+        hexgrid.features[x].properties.count = 0;
     }
-  }
+    console.log(hexgrid.features.length);
+    for (var y = 0; y < Object.keys(hexgrid.features).length;) {
+        count = 0;
+        for (var c = 0; c < filteredAmenity.length; c++) {
+            var poly = turf.polygon(hexgrid.features[y].geometry.coordinates);
+            if (filteredAmenity[c].geometry["type"] == "Polygon") {
+                if (turf.inside(turf.centroid(filteredAmenity[c]), poly)) {
+                    hexgrid.features[y].properties.count += 1;
+                    count++;
+                }
+            }
 
-  }
-  if(count == 0) {
-    hexgrid.features.splice( y, 1 );
-  }
-  else {
-    y++;
-  }
-  }
+            if (filteredAmenity[c].geometry["type"] == "Point") {
+                if (turf.inside(filteredAmenity[c], poly)) {
+                    hexgrid.features[y].properties.count += 1;
+                    count++;
+                }
+            }
+
+        }
+        if (count == 0) {
+            hexgrid.features.splice(y, 1);
+        } else {
+            y++;
+        }
+    }
 }
